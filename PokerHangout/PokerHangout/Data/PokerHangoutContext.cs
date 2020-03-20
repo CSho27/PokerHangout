f4129c26c7b83b8f541035467a552899d730cb69
CSho27 <30477054+CSho27@users.noreply.github.com>
2020-03-20 10:26:28 -0400

    Setting up page

---
+++ PokerHangout/PokerHangout/Data/PokerHangoutContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PokerHangout.Data
+{
+    public class PokerHangoutContext : DbContext
+    {
+        public PokerHangoutContext (DbContextOptions<PokerHangoutContext> options)
+            : base(options)
+        {
+        }
+    }
+}
